8aa8dbd5206bdee9864ae8fa4f27d8a3b1351e9d
Dmitry <daguminov@edu.hse.ru>
2023-01-27 20:23:28 +0300

    3rd commit

--- Graph.h
+++ Graph.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#include <map>
+
 
 template<typename key_type, typename value_type, typename weight_type>
 class Graph {
+    class Node {
+        value_type value;
+        std::map<key_type, weight_type> edges;
+    };
+
+    std::map<key_type, Node> graph;
+
+public:
+
     
+
+
 };
 
