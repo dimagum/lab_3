3e66e977e54455eb804d98c9f4901436775c773e
Dmitry <daguminov@edu.hse.ru>
2023-01-27 20:57:42 +0300

    4th commit

--- Graph.h
+++ Graph.h
@@ -2,20 +2,49 @@
 
 #include <map>
 
-
+/*!
+ *  \brief Шаблонный класс графа
+ */
 template<typename key_type, typename value_type, typename weight_type>
 class Graph {
+    /*!
+    *  \brief Внутренний класс узла
+    */
     class Node {
         value_type value;
         std::map<key_type, weight_type> edges;
+
+    public:
+
+        /*!
+        *  \brief Дефолтный конструктор
+        */
+        Node() {
+            value = 0;
+            edges = std::map<key_type, weight_type>();
+        }
     };
 
     std::map<key_type, Node> graph;
 
 public:
-
-    
-
-
+    /*!
+    * \brief Дефолтный конструктор
+    */
+    Graph() {
+        graph = std::map<key_type, Node>();
+    }
+    /*!
+     * \brief Конструктор копирования
+     */
+    Graph(const Graph<key_type, value_type, weight_type>& other) {
+        graph = other.graph;
+    }
+    /*!
+     * \brief Конструктор перемещения
+     */
+    Graph(Graph<key_type, value_type, weight_type>&& other) noexcept {
+        graph = other.graph;
+    }
 };
 
---
+++ lab_3 documentation/documentation/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['graph_0',['Graph',['../class_graph.html#ad145158a040cc16d4c3ee035923cdb30',1,'Graph::Graph()'],['../class_graph.html#aee6baa5e97b3ba148659d781efda4c76',1,'Graph::Graph(const Graph&lt; key_type, value_type, weight_type &gt; &amp;other)']]]
+];
