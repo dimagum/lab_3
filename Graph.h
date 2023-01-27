bdb1977f9757ef7478e102602c244454c60c0fc1
Dmitry <daguminov@edu.hse.ru>
2023-01-27 20:17:37 +0300

    2nd commit

---
+++ Graph.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+template<typename key_type, typename value_type, typename weight_type>
+class Graph {
+    
+};
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Graph.h"
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
